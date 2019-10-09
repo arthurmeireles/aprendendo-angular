53441cb4580488de2212fade3c75f0697e7d1402
Arthur Meireles da Silva <arthur.meireles@escolar.ifrn.edu.br>
2019-10-08 22:11:40 -0300

    atualizando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { NgifNgforComponent } from './ngif-ngfor/ngif-ngfor.component';
 
 
 const routes: Routes = [
   { path: 'componente-inicial', component: DataBindingComponent },
   { path: 'segundo-componente', component: SegundoComponenteComponent },
+  { path: 'ngif-ngfor', component: NgifNgforComponent},
 
 ];
 
